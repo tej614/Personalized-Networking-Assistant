1d1848ac0c2a11d6a8df4cc657a046b33a8e8738
Tej Gunturi <tejgunturi@gmail.com>
2026-07-01 22:38:34 +0530

    Updated topic_generator.py verified

--- app/services/topic_generator.py
+++ app/services/topic_generator.py
@@ -3,7 +3,7 @@ from app.models.schemas import Event
 def generate_topics(event:Event):
     topics=[
         f"Latest trends in {event.domain}",
-        f"Career opurtunities in {event.domain}",
+        f"Career opportunities in {event.domain}",
         f"Innovations in {event.domain}"
     ]
     return topics
