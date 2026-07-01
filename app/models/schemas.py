1b7ac3ca8abf1835ec3e0809b79e27bdacf21b10
Tej Gunturi <tejgunturi@gmail.com>
2026-07-01 08:30:20 +0530

    Updated schemas.py validation

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,6 +1,21 @@
 from pydantic import BaseModel
 from typing import List
+
 class User(BaseModel):
-    name:str
-    profession:str
-    intrests:List[str]
+    name: str
+    profession: str
+    interests: List[str]
+
+class Event(BaseModel):
+    title: str
+    domain: str
+    location: str
+
+class ConversationRequest(BaseModel):
+    user:User
+    event:Event
+    
+class ConversationResponse(BaseModel):
+    suggested_topics:List[str]
+    networking_tips:List[str]
+    fact_check_status:str
